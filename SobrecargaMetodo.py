854209c4ce67d36193651b42acaa26f54773e65d
Chricd51 <tonhpon057@gmail.com>
2025-05-03 19:48:49 -0500

    Update SobrecargaMetodo.py

--- SobrecargaMetodo.py
+++ SobrecargaMetodo.py
@@ -2,7 +2,7 @@ class Calculadora:
     def sumar(self, a, b, c=0):
         return a + b + c
 
-# Ejemplo de uso
+#Ejemplo
 calc = Calculadora()
-print(calc.sumar(5, 10))       # Suma de 2 valores
-print(calc.sumar(5, 10, 15))   # Suma de 3 valores
+print(calc.sumar(5, 10)) #Suma de 2 valores
+print(calc.sumar(5, 10, 15)) #Suma de 3 valores
