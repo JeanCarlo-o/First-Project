9f84792161bf60dac5cf17216c36fd223d60317d
Chricd51 <tonhpon057@gmail.com>
2025-05-03 19:47:11 -0500

    Agregado ĺa sobrecarga de metodos

---
+++ SobrecargaMetodo.py
@@ -0,0 +1,8 @@
+class Calculadora:
+    def sumar(self, a, b, c=0):
+        return a + b + c
+
+# Ejemplo de uso
+calc = Calculadora()
+print(calc.sumar(5, 10))       # Suma de 2 valores
+print(calc.sumar(5, 10, 15))   # Suma de 3 valores
